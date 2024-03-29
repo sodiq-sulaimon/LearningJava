0ccf79b355461c21ea3135ed2680ad2314a51054
Sodiq Sulaimon <sodiq.sulaimon204@gmail.com>
2024-03-29 12:44:02 -0500

    Added ConstantTest dir with files

--- ConstantTest/ConstantTest.java
+++ ConstantTest/ConstantTest.java
@@ -4,6 +4,6 @@ with 'final' keyword */
 public class ConstantTest{
     public static void main(String[] args){
         final double PI = 3.14159265359;
-        PI = 3.14 // generates compiler error because PI cannot be assigned a new value
+        PI = 3.14; // generates compiler error because PI cannot be assigned a new value
     }
 }
