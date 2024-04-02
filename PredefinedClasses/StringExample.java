3a9a9b1458134d49995a43b87665e607e5b2cdd8
Sodiq Sulaimon <sodiq.sulaimon204@gmail.com>
2024-04-02 11:59:01 -0500

    Added PredefinedClasses dir and files

---
+++ PredefinedClasses/StringExample.java
@@ -0,0 +1,15 @@
+public class StringExample{
+    public static void main(String[] args){
+        String major;
+        major = new String("Computer Science");
+        int charLength = major.length();
+        String toLower = major.toLowerCase();
+
+        System.out.println(major);
+        System.out.println(charLength);
+        System.out.println(toLower);
+
+    }
+}
+
+
