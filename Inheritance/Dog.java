eba9597c9ad7fc9ac6050491b015bb7455500c16
Sodiq Sulaimon <sodiq.sulaimon204@gmail.com>
2024-04-23 10:29:48 -0500

    Modified Dog.java: created an 'equals' method.

--- Inheritance/Dog.java
+++ Inheritance/Dog.java
@@ -6,13 +6,28 @@ public class Dog extends Canine{
         this.size = size;
     }
 
-        public void fetch(){
+    public void fetch(){
         System.out.println("Run");
         System.out.println("Clinch");
         System.out.println("Fetch");
     }
+
+    public void groom(){};
+
+    public boolean equals(Object obj){
+        if (!(obj instanceof Dog)) {
+            return false;
+        }
+        Dog doggy = (Dog) obj;
+        return ((doggy.size == size) && (doggy.name.equals(name)));
+    }
+
     public static void main(String[] args){
-        Dog spot = new Dog("Spot", 5.8);
-        spot.bark();
+        Dog spot1 = new Dog("Spot", 5.90001);
+        Dog spot2 = new Dog("Spot", 5.9);
+        spot1.bark();
+        spot2.bark();
+
+        System.out.println(spot1.equals(spot2));
     }
 }
