a5afe76cd85c6d290dcc7f43aca7710ddb168037
Sodiq Sulaimon <sodiq.sulaimon204@gmail.com>
2024-04-30 12:32:02 -0500

    Implemented emergencyMeeting method

--- Homework/Interface/RedAstronaut.java
+++ Homework/Interface/RedAstronaut.java
@@ -1,8 +1,10 @@
+import java.util.Arrays;
+
 public class RedAstronaut extends Player implements Impostor {
     private String skill;
 
-    public final int DEFAULT_SUSLEVEL = 15;
-    public final String DEFAULT_SKILL = "experienced";
+    public static final int DEFAULT_SUSLEVEL = 15;
+    public static final String DEFAULT_SKILL = "experienced";
 
     public RedAstronaut(String name, int susLevel, String skill) {
         super(name, susLevel);
@@ -14,17 +16,37 @@ public class RedAstronaut extends Player implements Impostor {
     }
 
     public void emergencyMeeting() {
-        Player p;
-        if (p.isFrozen) {
-            return;
+        if (!super.isFrozen()) {
+            Player[] players = super.getPlayers();
+            Arrays.sort(players);
+            int maxSusLevel = (players[players.length-1]).getSusLevel(); // the last element in the sorted array will have the max SusLevel
+            int maxSusLevelCount = 0;
+
+            for (Player player : players) {
+                if (!player.isFrozen() && player.getSusLevel() == maxSusLevel) {
+                maxSusLevelCount++;
+                }
+            }
+            for (Player player : players) {
+                if (!getName().equals(player.getName()) && maxSusLevelCount == 1) {
+                    player.setFrozen(true);
+                }
+                else
+                    return;
+            }
         }
+        super.gameOver();
     }
 
     public void sabotage(Player p) {
+        return;
+    }
 
+    public void freeze(Player p) {
+        return;
     }
 
     public boolean equals(Object o) {
-        super.equals();
+        return false;
     }
 }
